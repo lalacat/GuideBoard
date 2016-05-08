48147a5b7a4d51c7a1a3a62fe0dc12c13ada9379
lalacat <scott.si@hotmail.com>
2016-05-08 19:30:26 +0800

    add first version

---
+++ GuideBoard/CommandFunction.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace GuideBoard
+{
+    public class CommandFunction
+    {
+        public static void ResetFun()
+        {
+            Console.WriteLine("ResetFun");
+        }
+
+        public static void DirectionFun()
+        {
+            Console.WriteLine("DirectionFun");
+
+        }
+
+        public static void RotateFun()
+        {
+            Console.WriteLine("RotateFun");
+
+        }
+
+        public static void ShowMessageFun()
+        {
+            Console.WriteLine("ShowMessageFun");
+
+        }
+
+        public static void AddKeyboardFun()
+        {
+            Console.WriteLine("AddKeyboardFun");
+
+        }
+
+        public static void DeleteKeyboardFun()
+        {
+            Console.WriteLine("DeleteKeyboardFun");
+
+        }
+
+        public static void QuireKeyboardFun()
+        {
+            Console.WriteLine("QuireKeyboardFun");
+
+        }
+
+        public static void ShowKeyboardFun()
+        {
+            Console.WriteLine("ShowKeyboardFun");
+
+        }
+
+        public static void KeyboardControlFun()
+        {
+            Console.WriteLine("KeyboardControlFun");
+
+        }
+
+        public static void WifiSetFun()
+        {
+            Console.WriteLine("WifiSetFun");
+
+        }
+            
+    }
+}
